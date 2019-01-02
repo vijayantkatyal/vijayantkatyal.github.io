56b3e145f4b2cae79bef67befd533084bde10722
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2019-01-02 13:03:48 +0530

    [Fixed] copy code on click

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -6,11 +6,11 @@ $(".code_title").click(function () {
 	// var _data = $(this).parents().next('.highlighter-rouge').find('.code').get(0);
 	
 	window.getSelection().removeAllRanges();
-	var _data = $(this).next('.highlighter-rouge').find('.code').get(0);
+	var _data = $(this).next('.highlighter-rouge').find('code').get(0);
 	var range = document.createRange();
 	range.selectNode(_data);
 	window.getSelection().addRange(range);
 	document.execCommand("Copy");
-	alert("text copied");
+	alert("code copied");
 	window.getSelection().removeAllRanges();
 });
