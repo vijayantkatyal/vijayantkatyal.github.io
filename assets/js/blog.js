d6c0cb05b75e42e06a9548950a2884e25f4440d0
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2019-01-06 04:15:46 +0530

    [Added] open all external links in new tab

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,12 @@
-mediumZoom('img')
+mediumZoom('img');
 
+jQuery(document).ready(function($) {
+	$('a')
+		.filter('[href^="http"], [href^="//"]')
+		.not('[href*="' + window.location.host + '"]')
+		.attr('rel', 'noopener noreferrer')
+		.attr('target', '_blank');
+});
 // $("img").hover(
 // 	function(){
 // 		$("body").addClass("dark");
